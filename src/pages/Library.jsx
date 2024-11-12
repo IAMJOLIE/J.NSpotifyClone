d4afc9cea48837ac4fff0c97bab3007e45fd8349
Julie Nalband <julienalband@Julies-MacBook-Air.local>
2024-11-12 22:20:30 +0100

    Fixar filväg för Playlistitem

--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Box, List, Typography } from '@mui/material';
-import PlaylistItem from '../component/Playlistitem/Playlistitem';
+
+import PlaylistItem from '../component/Playlistitem/Playlistitem.jsx';
 
 const Library = ({ spotifyApi, token }) => {
 	const [playList, setPlayList] = useState(null);
