9243194a8490896e08fb041f012c688ae4d05888
Julie Nalband <julia_157nalband@outlook.com>
2024-11-20 18:51:49 +0100

    lägga till loading-state för att få tillgång till api från spotify

--- src/component/Dashboard.jsx
+++ src/component/Dashboard.jsx
@@ -10,10 +10,16 @@ import MobilNav from './MobilNav/MobilNav';
 import Library from '../pages/Library';
 
 const Dashboard = ({ spotifyApi }) => {
+	const [isLoading, setIsLoading] = useState(true);
 	const [token, setToken] = useState(getAccessTokenFromStorage());
 	useEffect(() => {
 		async function onMount() {
+		
 			await spotifyApi.setAccessToken(token);
+			
+			setIsLoading(false)
+
+
 		}
 		if (token) {
 			onMount();
@@ -22,15 +28,19 @@ const Dashboard = ({ spotifyApi }) => {
 
 	return (
 		<Box sx={{ width: '100%', height: '100%', display: 'flex', flexDirection: 'column' }}>
+			{!isLoading && 
 			<Box sx={{ flex: 1, overflowY: 'auto', display: 'flex' }}>
-				<SidNav spotifyApi={spotifyApi} token={token} />
-				<Routes>
-					<Route path="/playlist/:id" element={<Playlist spotifyApi={spotifyApi} token={token} />} />
-					<Route path="/Library" element={<Library spotifyApi={spotifyApi} token={token} />} />
-					<Route path="/" element={<Home />} />
-				</Routes>
-			</Box>
-			{token && <Player spotifyApi={spotifyApi} token={token} />}
+			<SidNav spotifyApi={spotifyApi} token={token} />
+			<Routes>
+				<Route path="/playlist/:id" element={<Playlist spotifyApi={spotifyApi} token={token} />} />
+				<Route path="/Library" element={<Library spotifyApi={spotifyApi} token={token} />} />
+				<Route path="/" element={<Home />} />
+			</Routes>
+		</Box>
+			 }
+			
+			
+			 {token &&  !isLoading  && <Player spotifyApi={spotifyApi} token={token} />} 
             <MobilNav/>
 		</Box>
 	);
